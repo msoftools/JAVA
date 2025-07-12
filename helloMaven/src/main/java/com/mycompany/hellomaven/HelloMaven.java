cb5afab8ca239afaeb74aea18e483dbd8c79ef44
MarioServer <MarioServer@DESKTOP-5GAAL6L>
2025-07-11 23:48:59 -0600

    hello maven

---
+++ helloMaven/src/main/java/com/mycompany/hellomaven/HelloMaven.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.hellomaven;
+
+/**
+ *
+ * @author MarioServer
+ */
+public class HelloMaven {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
