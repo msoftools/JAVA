e92015ed8ed34245bd38a34f2d5f62b31b592ea0
msoftools <msoftools@gmail.com>
2025-08-01 23:33:24 -0600

    third

--- helloMarioAnt/src/hellomarioant/HelloMarioAnt.java
+++ helloMarioAnt/src/hellomarioant/HelloMarioAnt.java
@@ -14,6 +14,6 @@ public class HelloMarioAnt {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-System.out.println("hola buen kike");    }
+System.out.println("hola buen enrique");    }
     
 }
